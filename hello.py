5f7290d081ed4713f6d49c313e7fca39376bfe27
brandon mazey <brandon@Mac.localdomain>
2026-09-23 09:38:26 -0400

    a witch is getting married

--- hello.py
+++ hello.py
@@ -2,4 +2,18 @@
 
 if __name__ == '__main__':
     # this is a comment
-    print('new dawn, new day!')
+    # print('new dawn, new day!')
+
+    sunny = True
+    rain = False
+
+    if sunny and rain:
+        print('a witch is getting married!')
+    elif rain:
+        print('it might rain today, bring your raincoat ...')
+    else:
+        print('weather looks sunny!')
+
+    # casting demo
+    
+    
