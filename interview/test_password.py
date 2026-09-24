0201a392683bfe1cdcdc93b9ab82f0c1f349f574
rospg <rpaniaguagarcia@stsaviour.org>
2026-09-24 08:39:04 -0400

    working on password tests

--- interview/test_password.py
+++ interview/test_password.py
@@ -5,6 +5,7 @@ def test_password_alpha_characters():
     # TODO BONUS ensure that the first 5 characters are letters
     #use regex to check if the first 5 are letters
     # assert!!!
+    password1 = generate_password()
     pass
 
 def test_password_numberic_characters():
@@ -20,13 +21,16 @@ def test_password_symbol_character():
 def test_password_length():
     # TODO BONUS ensure the length of the password is 10
     # check len(str) == 10
-    pass
+    password1 = generate_password()
+    assert len(password1) == 10
 
 def test_password_unique():
     # HINT we ignore collisions for the purposes of this exercise
     # TODO BONUS create two passwords, and ensure they are distinct
     # use password method to create 2 passwords, use == to check if they are equal
-    pass
+    password1 = generate_password()
+    password2 = generate_password()
+    assert password1 != password2
 
 def contains(s: str, collection: list):
     # check if any characters in collection are present in s
